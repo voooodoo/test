99319c6389baa153ed5c996d230cdb915c9338ba
eugene <evgen.zaharevich@gmail.com>
2018-08-16 08:49:03 +0300

    optimization 1-2

--- src/app/book/form/form.component.ts
+++ src/app/book/form/form.component.ts
@@ -10,60 +10,33 @@ import { HttpService } from '../../http.service';
 export class FormComponent implements OnInit {
 
 @Input() id:number;
+hasFormInit:boolean = false;
+form: FormGroup;
 
 data = {
   books: {
     url: '',
-    data: {},
-    hasData: false
+    data: []
   },
   formats: {
     url: '',
-    data:{},
-    hasData: false
+    data:[]
   },
   countries: {
     url: '',
-    data:'',
-    hasData: false
+    data:[]
   }, 
   cities: {
     url: '',
-    data:{},
-    hasData: false
+    data:[]
   }, 
   companies: {
     url: '',
-    data:{},
-    hasData: false
+    data:[]
   } 
 }
 
-urlFormats: string;
-formatsData: any[];
-
-urlCountries: string;
-countriesData: any;
-
-urlCities: string;
-citiesData: any;
-
-urlPublishers: string;
-publishersData: any;
-
-urlBooks: string;
-booksData: any;
-
-form: FormGroup;
-defaultValue:any;
-
   constructor(private fb:FormBuilder, private httpService: HttpService) {
-    /*this.urlFormats = 'http://localhost:3004/formats';
-    this.urlCountries = 'http://localhost:3004/countries';
-    this.urlCities = 'http://localhost:3004/cities';
-    this.urlPublishers = 'http://localhost:3004/companies';
-    this.urlBooks = 'http://localhost:3004/books';
-    */
     this.data.books.url = 'http://localhost:3004/books';
     this.data.formats.url = 'http://localhost:3004/formats';
     this.data.countries.url = 'http://localhost:3004/countries';
@@ -72,13 +45,6 @@ defaultValue:any;
   }
 
   ngOnInit() {
-    //this.httpService.getData(this.urlBooks).subscribe((data:any[]) => this.storeBooksData(data) );
-    //this.httpService.getData(this.urlFormats).subscribe((data:any[]) => this.storeFormatsData(data) );
-    //this.httpService.getData(this.urlCountries).subscribe((data:any[]) => this.storeCountriesData(data) );
-    //this.httpService.getData(this.urlCities).subscribe((data:any[]) => this.storeCitiesData(data) );
-    //this.httpService.getData(this.urlPublishers).subscribe((data:any[]) => this.storePublishersData(data) ); 
-    //if (this.id) {this.httpService.getData(this.data.books.url).subscribe((data:any[]) => this.setData(this.data.books, data))}
-
     for(let item in this.data) {
       let elem = this.data[item];
       this.httpService.getData(elem.url).subscribe((data:any[]) => this.setData(elem, data))
@@ -100,15 +66,19 @@ defaultValue:any;
   
   setData(target, data) {
     target.data = data;
-    if(this.id){
-      /*if(target.data) {target.hasData = true;}
-      for(let item in this.data) { 
-        let arr= []
-        arr.push(this.data[item]['hasData'])
-        else {
-          this.setFormValue(this.id)//build form
-          return
-        }*/
+    if(this.id && !this.hasFormInit) {
+      let hasAllData = true;
+      
+      for(let item in this.data) {
+        if(this.data[item].data.length==0) {
+          hasAllData = false;
+        }    
+      }
+
+      if(hasAllData) {
+        this.hasFormInit = true;
+        this.setFormValue(this.id);
+      }
     }
   }
 
@@ -123,18 +93,13 @@ defaultValue:any;
       this.form.controls['price'].setValue(book.price);
       
       let formatId = this.data.books.data[id].formatId;
-      console.log(formatId);
       let format = this.data.formats.data[formatId-1].name;
-      console.log(format);
-      //console.log(f)
+      this.form.controls['Format'].setValue(format);
       
-      //this.form.controls['Format'].setValue(format);
-      
-      /*
       let countryId = this.data.books.data[id].countryId;
-      let country = this.data.countries[countryId-1].name;  
+      let country = this.data.countries.data[countryId-1].name;  
       this.form.controls['Country'].setValue(country);
-
+      
       let cityId = this.data.books.data[id].cityId;
       let city = this.data.cities.data[cityId-1].name;
       this.form.controls['City'].setValue(city);
@@ -142,62 +107,8 @@ defaultValue:any;
       let companyId = this.data.books.data[id].companyId;
       let company = this.data.companies.data[companyId-1].name;
       this.form.controls['Company'].setValue(company);
-      */
   }
 
-  
-
-  
-/*
-  storeFormatsData (data) {
-    this.formatsData = data;
-    if(this.id) {
-      let formatId = this.booksData[this.id-1].formatId;
-      let format = this.formatsData[formatId-1].name;
-      this.form.controls['Format'].setValue(format);
-    }
-  }
-
-  storeCountriesData (data) {
-    this.countriesData = data;
-    if(this.id) {
-        let countryId = this.booksData[this.id-1].countryId;
-        let country = this.countriesData[countryId-1].name;  
-        this.form.controls['Country'].setValue(country);
-    }
-  }
-
-  storeCitiesData (data) {
-    this.citiesData = data;
-    if(this.id) {
-      let cityId = this.booksData[this.id-1].cityId;
-      let city = this.citiesData[cityId-1].name;
-      this.form.controls['City'].setValue(city);
-    }
-  }
-
-  storePublishersData (data) {
-    this.publishersData = data;
-    if(this.id) {
-      let companyId = this.booksData[this.id-1].companyId;
-      let company = this.publishersData[companyId-1].name;
-      this.form.controls['Company'].setValue(company);
-    }
-  }
-
-  storeBooksData(data) {
-    this.booksData = data;
-    if(this.id) {
-      let book = this.booksData[this.id-1];
-      this.form.controls['author'].setValue(book.author);
-      this.form.controls['title'].setValue(book.title);
-      this.form.controls['isbn'].setValue(book.isbn);
-      this.form.controls['pages'].setValue(book.pages);
-      this.form.controls['description'].setValue(book.description);
-      this.form.controls['price'].setValue(book.price);
-    }
-  }
-  */
   onSubmit(form) {
     console.log(form);
   }
